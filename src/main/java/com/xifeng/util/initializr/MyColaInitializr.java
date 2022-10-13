f923baab3d54856a65bbb6ce80a02737da3d4026
xiezb <xiezhibin@camelfin.com>
2022-10-13 14:37:09 +0800

    add

---
+++ src/main/java/com/xifeng/util/initializr/MyColaInitializr.java
@@ -0,0 +1,23 @@
+/**
+ * Project Name:MyUtil <br/>
+ * File Name:MyColaInitializr.java <br/>
+ * Package Name:com.xifeng.util.initializr <br/>
+ * @author xiezb
+ * Date:2022年10月13日下午2:36:36 <br/>
+ * Copyright (c) 2022, 驼驼数科 Rights Reserved.
+ */
+
+package com.xifeng.util.initializr;
+/**
+ * ClassName: MyColaInitializr
+ * Description: Cola架构 工程自定义生成器
+ * @author xiezb
+ * Date:2022年10月13日下午2:36:36 <br/>
+ * @version
+ * @since JDK 1.8
+ */
+public class MyColaInitializr {
+
+}
+
+	
